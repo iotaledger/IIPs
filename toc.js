cc27a0378a0bb782ce75c12a0946c091d2a1a658
cyberphysic4l <cyberphysic4l@users.noreply.github.com>
2025-07-03 10:13:04 +0000

    deploy: 41cbd7db9d11dd0f5ee6dc0fe472b4494cb2a1bb

--- toc.js
+++ toc.js
@@ -8,9 +8,9 @@ class MDBookSidebarScrollbox extends HTMLElement {
         super();
     }
     connectedCallback() {
-        this.innerHTML = '<ol class="chapter"><li class="chapter-item expanded affix "><a href="introduction.html">Introduction</a></li><li class="chapter-item expanded "><a href="iips/IIP-0001/iip-0001.html">IIP-1 IIP Process</a></li><li class="chapter-item expanded "><a href="iips/IIP-0002/iip-0002.html">IIP-2 Starfish Consensus Protocol</a></li></ol>';
+        this.innerHTML = '<ol class="chapter"><li class="chapter-item expanded affix "><a href="introduction.html">Introduction</a></li><li class="chapter-item expanded "><a href="iips/IIP-0001/iip-0001.html">IIP-1 IIP Process</a></li><li class="chapter-item expanded "><a href="iips/IIP-0002/iip-0002.html">IIP-2 Starfish Consensus Protocol</a></li><li class="chapter-item expanded "><a href="iips/IIP-0003/iip-0003.html">IIP-3 Sequencer Improvements</a></li></ol>';
         // Set the current, active page, and reveal it if it's hidden
-        let current_page = document.location.href.toString().split("#")[0];
+        let current_page = document.location.href.toString().split("#")[0].split("?")[0];
         if (current_page.endsWith("/")) {
             current_page += "index.html";
         }
