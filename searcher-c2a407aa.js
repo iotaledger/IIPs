39c85895b30321cafaa223e830adcbe7e813e727
miker83z <miker83z@users.noreply.github.com>
2026-03-16 08:32:53 +0000

    deploy: 5434eeb8e8d5834f65ccc4831189993c96de03c0

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-44f303dc.js',
+                path_to_root + 'searchindex-a77fe9ce.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
