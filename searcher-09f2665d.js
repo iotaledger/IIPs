252b14d7a7ed0efbdf82595c95985ab334fb4dac
lollobene <lollobene@users.noreply.github.com>
2026-06-16 10:05:25 +0000

    deploy: 49742841567af7165546c2a8c8722f51f884c6ca

--- searcher-09f2665d.js
+++ searcher-09f2665d.js
@@ -440,7 +440,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-c06d470d.js',
+                path_to_root + 'searchindex-e2a8cbf9.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
